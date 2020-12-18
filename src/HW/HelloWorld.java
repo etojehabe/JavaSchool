e12538f59385706db93645967a6d27203faf4410
PetrovR <sdngliv@gmail.com>
2020-12-18 22:07:11 +0400

    refix conflict

--- src/HW/HelloWorld.java
+++ src/HW/HelloWorld.java
@@ -2,10 +2,6 @@ package HW;
 
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hi people");
-=======
-        System.out.println("Hi people");
->>>>>>> third
     }
 }
